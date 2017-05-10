d2102461b75d731553844b4769a56e0a46b2a281
mixxxxx <spartacusxxxxx@gmail.com>
2017-05-10 21:24:21 +0300

    arrangeBiggestNumber.php исправил ошибку в function delElemMass(, )

--- arrangeBiggestNumber.php
+++ arrangeBiggestNumber.php
@@ -25,9 +25,8 @@ function compareTwoNumber($firstNumber, $secondNumber)
 function delElemMass($arr, $selected_val)
 {
 	$value_to_delete = $selected_val;
-	$arr = array_flip($arr);
-	unset ($arr[$value_to_delete]);
-	$arr = array_flip($arr);
+	$key = array_search($value_to_delete, $arr);       
+	unset ($arr[$key]);
 	return $arr;
 }
 
@@ -41,6 +40,9 @@ function My_arrangeBiggestNumber($arr)
 		foreach ($arr as &$value) 
 		{
 			$selected_val = compareTwoNumber($selected_val, $value);
+			//echo "selected_val is ";
+			//print_r($selected_val);
+			//echo "<br>";
 		}
 
 		//print_r($selected_val);
@@ -48,13 +50,18 @@ function My_arrangeBiggestNumber($arr)
 		unset($value);
 
 		$concat_value = $concat_value . $selected_val;
+		//echo "concat_value is ";
+		//print_r($concat_value);
+		//echo "<br>";
 
+		//echo "arr is ";
 		//print_r($arr);
 		//echo "<br>";
 
 		$arr = delElemMass($arr, $selected_val);
 		$arr = array_values($arr);
 
+		//echo "arr after del is ";
 		//print_r($arr);
 		//echo "<br>";
 	}
@@ -67,7 +74,8 @@ function My_arrangeBiggestNumber($arr)
 
 echo "arrangeBiggestNumber of array [1, 34, 3, 98, 9, 76, 45, 4] ";
 echo "<br>";
-My_arrangeBiggestNumber([1, 34, 3, 98, 9, 76, 45, 4]);
+//My_arrangeBiggestNumber([1, 34, 3, 98, 9, 76, 45, 4]);
+My_arrangeBiggestNumber([0,2,0]);
 
 
 ?>
