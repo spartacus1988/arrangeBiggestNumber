226b2941a358130d8cfabd2b0c34ba3700f0b174
mixxxxx <spartacusxxxxx@gmail.com>
2017-05-12 12:16:41 +0300

    twoAsyncFunc.php

---
+++ twoAsyncFunc.php
@@ -0,0 +1,49 @@
+
+<html>
+	<body>
+	    <h1>Header_h1</h1>
+	    <script type="text/javascript" charset="UTF-8">
+
+			var a = undefined, b = undefined;
+			var ajaxFunctionsAmount = 2;
+
+			function tryFinish() { 
+   				ajaxFunctionsAmount --;  
+   				if(ajaxFunctionsAmount === 0) { 
+      			report(); 
+   				} 
+			}
+
+			function getA(callback)
+			{
+				setTimeout(function(){
+    				a = 'a';
+    				callback();		
+				}, 1000 * Math.random());
+			}
+
+			function getB(callback)
+			{
+				setTimeout( function(){
+    				b = 'b';
+    				callback();
+				}, 1000 * Math.random());
+			}
+
+			getA(tryFinish);
+			getB(tryFinish);
+
+			function report()
+			{
+				console.log(a);
+				console.log(b);
+			}
+		</script>
+	    <h1>End</h1>
+	</body>
+</html>
+
+
+
+
+
